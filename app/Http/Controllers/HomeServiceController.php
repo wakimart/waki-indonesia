ab6680cf6a73c731275c7bc6f22ae6878ddb2854
yasir161 <myasiraa16>
2020-07-13 13:18:00 +0700

    fixing google calender

--- app/Http/Controllers/HomeServiceController.php
+++ app/Http/Controllers/HomeServiceController.php
@@ -11,6 +11,7 @@ use App\Utils;
 use Carbon\Carbon;
 use App\Exports\HomeServicesExport;
 use Maatwebsite\Excel\Facades\Excel;
+use Google_Service_Calendar_Event;
 
 
 class HomeServiceController extends Controller
@@ -128,6 +129,37 @@ class HomeServiceController extends Controller
         $data['appointment'] = $data['date']." ".$data['time'];
         $order = HomeService::create($data);
     
+        $event = new Google_Service_Calendar_Event(array(
+            'summary' => 'Google I/O 2015',
+            'location' => '800 Howard St., San Francisco, CA 94103',
+            'description' => 'A chance to hear more about Google\'s developer products.',
+            'start' => array(
+              'dateTime' => '2021-05-28T09:00:00-07:00',
+              'timeZone' => 'America/Los_Angeles',
+            ),
+            'end' => array(
+              'dateTime' => '2021-05-28T17:00:00-07:00',
+              'timeZone' => 'America/Los_Angeles',
+            ),
+            'recurrence' => array(
+              'RRULE:FREQ=DAILY;COUNT=2'
+            ),
+            'attendees' => array(
+              array('email' => 'rayas143120@gmail.com'),
+              array('email' => 'myasiraa16@gmail.com'),
+            ),
+            'reminders' => array(
+              'useDefault' => FALSE,
+              'overrides' => array(
+                array('method' => 'email', 'minutes' => 24 * 60),
+                array('method' => 'popup', 'minutes' => 10),
+              ),
+            ),
+          ));
+          
+          $calendarId = 'primary';
+          $event = $service->events->insert($calendarId, $event);
+          
         return response()->json(['success' => 'Berhasil']);
     }
 
