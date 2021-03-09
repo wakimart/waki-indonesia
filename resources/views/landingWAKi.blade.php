e35acdee7bdbd750e5bc2e07888818063b98c586
frezznezz <jatmikogunawan@gmail.com>
2021-03-09 14:11:44 +0700

    add notif success

--- resources/views/landingWAKi.blade.php
+++ resources/views/landingWAKi.blade.php
@@ -15,6 +15,16 @@
 ============================================= -->
 <link href="https://fonts.googleapis.com/css?family=Poppins:300i,400,400i,500,600,700,800%7CLora:400,400i,700,700i%7COpen+Sans:800" rel="stylesheet">
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MMM76L65PE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MMM76L65PE');
+</script>
+
 <!-- Stylesheets
 ============================================= -->
 <link href="{{asset('css/landing/external.css')}}?ver=1.0" rel="stylesheet">
@@ -29,11 +39,150 @@
 <style>
 .WAKI{
   color: #00844a;
-    font-size: 68px;
-    line-height: 1;
-    font-weight: bolder;
-    -webkit-text-stroke: 1.5px black;
+  font-size: 68px;
+  line-height: 1;
+  font-weight: bolder;
+  -webkit-text-stroke: 1.5px black;
 }
+.testimonial {
+  background-color: white;
+  text-align: center;
+  padding: 20px 10px 40px;
+  margin: 0 15px 10px;
+  position: relative;
+  border-radius: 1em;
+}
+
+.testimonial .icon {
+  display: inline-block;
+  font-size: 80px;
+  color: #016d9b;
+  margin-bottom: 20px;
+  opacity: 0.6;
+}
+
+.testimonial .description {
+  font-size: 14px;
+  color: #777;
+  text-align: justify;
+  margin-bottom: 30px;
+}
+
+.testimonial .testimonial-content {
+  width: 100%;
+  left: 0;
+  position: absolute;
+}
+
+.testimonial .pic {
+  display: inline-block;
+  border: 4px solid white;
+  overflow: hidden;
+  z-index: 1;
+  position: relative;
+}
+
+.testimonial .pic img {
+  width: 100%;
+  height: auto;
+}
+
+.testimonial .name {
+  font-size: 15px;
+  font-weight: bold;
+  color: #2d2d2d;
+  text-transform: capitalize;
+  margin: 10px 0 5px 0;
+}
+
+.testimonial .title {
+  display: block;
+  font-size: 14px;
+  color: #ffd9b8;
+}
+
+.owl-controls {
+  margin-top: 20px;
+}
+
+.owl-pagination {
+  display: flex;
+  justify-content: center;
+}
+
+.owl-page {
+  height: 10px;
+  width: 40px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 10%;
+}
+
+.owl-page:hover,
+.owl-page.active {
+  background-color: rgba(255, 255, 255, 0.3);
+}
+
+.owl-page:not(first-item) {
+  margin-left: 10px;
+}
+
+.owl-carousel {
+-ms-touch-action: none;
+touch-action: none;
+}
+
+.modal-window {
+	 position: fixed;
+	 background-color: rgba(255, 255, 255, 0.25);
+	 top: 0;
+	 right: 0;
+	 bottom: 0;
+	 left: 0;
+	 z-index: 999;
+	 visibility: hidden;
+	 opacity: 0;
+	 pointer-events: none;
+	 transition: all 0.3s;
+}
+ .modal-window:target {
+	 visibility: visible;
+	 opacity: 1;
+	 pointer-events: auto;
+}
+ .modal-window > div {
+	 width: 400px;
+	 position: absolute;
+   bottom: 2em;
+	 left: 2em;
+	 padding: 2em;
+	 background: white;
+   border-radius: 1em;
+   background-color: #ccffeb;
+}
+ .modal-window header {
+	 font-weight: bold;
+}
+ .modal-window h5 {
+	 font-size: 16px;
+   margin: 0;
+   font-weight: 600;
+}
+ .modal-close {
+	 color: #aaa;
+	 line-height: 50px;
+	 position: absolute;
+	 right: 0;
+	 text-align: center;
+	 top: 0;
+	 width: 70px;
+	 text-decoration: none;
+   font-weight: bold;
+   font-family: "Open Sans", sans-serif;
+}
+ .modal-close:hover {
+	 color: black;
+}
+
 @media (min-width: 768px) {
   .mobadjust {
     display: flex;
@@ -313,8 +462,100 @@
     <!-- .container end -->
 </section>
 
+<!-- Testimoni #1
+============================================= -->
+<section id="portfolio" class="section portfolio portfolio-grid portfolio-2 pb-70">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
+                <div class="heading text-center mb-70">
+                    <h2 class="heading--title">Testimonial WAKi</h2>
+                </div>
+            </div>
+            <!-- .col-md-6 end -->
+        </div>
+
+        <div class="row">
+            <div id="testimonial-slider" class="owl-carousel">
+              <div class="testimonial">
+                <div class="pic">
+                  <img src="{{asset('sources/landing/testimonials/1.jpg')}}" alt="">
+                </div>
+                <p class="description">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </p>
+                <div class="testimonial-content">
+                  <h3 class="name">Michele Miller</h3>
+                </div>
+              </div>
+              <div class="testimonial">
+                <div class="pic">
+                  <img src="{{asset('sources/landing/testimonials/2.jpg')}}" alt="">
+                </div>
+                <p class="description">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </p>
+                <div class="testimonial-content">
+                  <h3 class="name">Patricia Knott</h3>
+                </div>
+              </div>
+              <div class="testimonial">
+                <div class="pic">
+                  <img src="{{asset('sources/landing/testimonials/3.jpg')}}" alt="">
+                </div>
+                <p class="description">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </p>
+                <div class="testimonial-content">
+
+                  <h3 class="name">Justin Ramos</h3>
+                </div>
+              </div>
+              <div class="testimonial">
+                <div class="pic">
+                  <img src="{{asset('sources/landing/testimonials/4.jpg')}}" alt="">
+                </div>
+                <p class="description">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </p>
+                <div class="testimonial-content">
+                  <h3 class="name">Mary Huntley</h3>
+                </div>
+              </div>
+              <div class="testimonial">
+                <div class="pic">
+                  <img src="{{asset('sources/landing/testimonials/5.jpg')}}" alt="">
+                </div>
+                <p class="description">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </p>
+                <div class="testimonial-content">
+                  <h3 class="name">Aaron Newell</h3>
+                </div>
+              </div>
+              <div class="testimonial">
+                <div class="pic">
+                  <img src="{{asset('sources/landing/testimonials/6.jpg')}}" alt="">
+                </div>
+                <p class="description">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </p>
+                <div class="testimonial-content">
+                  <h3 class="name">Lizzie Geren</h3>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <!-- .row end -->
+    </div>
+    <!-- .container end -->
+</section>
+<!-- #Testimoni end -->
+
 <!-- Footer #9
 ============================================= -->
+
 <footer id="footer9" class="footer footer-5 footer-8 footer-9 bg-white text-center-xs pt-0 pb-0">
 
     <!-- .footer-widget end -->
@@ -380,6 +621,19 @@
     </div>
 </div>
 
+<div class="container">
+  <div class="interior">
+    <a class="btn regisnotif" href="#open-modal">Basic CSS-Only Modal</a>
+  </div>
+</div>
+<div id="open-modal" class="modal-window">
+  <div>
+    <a href="#" title="Close" class="modal-close">X</a>
+    <h5>Jatmiko Gunawan telah berhasil mendaftar WAKi dirumah saja !</h5>
+    <!-- <div>A CSS-only modal based on the :target pseudo-class. Hope you find it helpful.</div> -->
+    </div>
+</div>
+
 </div>
 <!-- #wrapper end -->
 
@@ -392,8 +646,31 @@
 <script src="{{asset('js/landing/functions.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.countdown/2.2.0/jquery.countdown.min.js"></script>
 <script>
+$(document).ready(function() {
+  $("#testimonial-slider").owlCarousel({
+    items: 4,
+    pagination: true,
+    autoplay: true,
+    loop: true,
+    navigation: true,
+    dots: false,
+    responsive:{
+      1000: {items: 4},
+      768: {items: 3},
+      640: {items: 2},
+      370: {items: 1}
+    }
+  });
+
+  $("#success-alert").show();
+  // $("#myWish").click(function showAlert() {
+  //   $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
+  //     $("#success-alert").slideUp(500);
+  //   });
+  // });
+});
 // set the date we're counting down to
-var target_date = new Date('Feb 28, 2021 00:00:00').getTime();
+var target_date = new Date('Mar 25, 2021 00:00:00').getTime();
 
 // variables for time units
 var days, hours, minutes, seconds;
@@ -426,9 +703,16 @@ setInterval(function () {
       + '<span class="h1 font-weight-bold">' + (seconds<10 ? "0" + seconds : seconds) + ' </span>';
 }, 1000);
 </script>
-<script type="text/javascript">
-    $("#modal-Success").modal("show");
+
+<script>
+setInterval(function () {
+   document.querySelector("a.regisnotif").click();
+}, 5000);
+setInterval(function () {
+   document.querySelector("a.modal-close").click();
+}, 6000);
 </script>
+
 @if(Session::has('success_registration'))
     <script type="text/javascript">
         $("#modal-Success").modal("show");
