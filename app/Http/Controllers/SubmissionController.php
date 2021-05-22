e96b94b61b42d387791d50571dc8d72c57b1161e
Andre Rahardjo <20762999+andrerahardjo97@users.noreply.github.com>
2021-05-22 13:34:10 +0700

    fix(Submission): Reformat code

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -27,7 +27,11 @@ class SubmissionController extends Controller
 {
     private function isJSON($string)
     {
-        return is_string($string) && is_array(json_decode($string, true)) && (json_last_error() == JSON_ERROR_NONE) ? true : false;
+        return is_string($string)
+            && is_array(json_decode($string, true))
+            && (json_last_error() === JSON_ERROR_NONE)
+            ? true
+            : false;
     }
 
     public function convertHsToForeign()
@@ -67,6 +71,10 @@ class SubmissionController extends Controller
             }
 
             DB::commit();
+
+            return response()->json([
+                "message" => "Link_HS berhasil dikonversi menjadi JSON.",
+            ]);
         } catch (Exception $e) {
             DB::rollBack();
 
