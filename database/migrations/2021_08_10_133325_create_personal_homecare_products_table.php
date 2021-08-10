859d5791c70ab6c204892a66f54b3223bf9ce19f
andrerahardjo97 <20762999+andrerahardjo97@users.noreply.github.com>
2021-08-10 15:12:14 +0700

    feat(Personal Homecare): Migration

---
+++ database/migrations/2021_08_10_133325_create_personal_homecare_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalHomecareProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_homecare_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("code", 20);
+            $table->unsignedInteger("branch_id");
+            $table->foreign("branch_id")->references("id")->on("branches");
+            $table->unsignedInteger("product_id");
+            $table->foreign("product_id")->references("id")->on("products");
+            $table->boolean("status");
+            $table->boolean("active");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal_homecare_products');
+    }
+}
---
+++ database/migrations/2021_08_10_133845_create_personal_homecares_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalHomecaresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_homecares', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum("status", [
+                "new",
+                "approve_out",
+                "process",
+                "waiting_in",
+                "done",
+            ]);
+            $table->date("schedule");
+            $table->string("name");
+            $table->string("phone", 20);
+            $table->string("address", 200);
+            $table->integer("province_id");
+            $table->integer("city_id");
+            $table->integer("subdistrict_id");
+            $table->unsignedInteger("branch_id");
+            $table->foreign("branch_id")->references("id")->on("branches");
+            $table->unsignedInteger("cso_id");
+            $table->foreign("cso_id")->references("id")->on("csos");
+            $table->string("id_card");
+
+            $table->unsignedInteger("checklist_out");
+            $table->foreign("checklist_out")
+                ->references("id")
+                ->on("personal_homecare_checklists");
+
+            $table->unsignedInteger("checklist_in")->nullable();
+            $table->foreign("checklist_in")
+                ->references("id")
+                ->on("personal_homecare_checklists");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal_homecares');
+    }
+}
