8cf33a88fee69f5346f57c2c6c6306eb792aa095
andrerahardjo97 <20762999+andrerahardjo97@users.noreply.github.com>
2021-08-20 14:41:32 +0700

    feat(Personal Homecare): Edit

--- app/Http/Controllers/PersonalHomecareController.php
+++ app/Http/Controllers/PersonalHomecareController.php
@@ -8,7 +8,9 @@ use App\HistoryUpdate;
 use App\PersonalHomecare;
 use App\PersonalHomecareChecklist;
 use App\PersonalHomecareProduct;
+use App\RajaOngkir_City;
 use App\RajaOngkir_Province;
+use App\RajaOngkir_Subdistrict;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -25,9 +27,10 @@ class PersonalHomecareController extends Controller
             ->with('i', (request()->input('page', 1) - 1) * 10);
     }
 
-    public function phForm($id){
+    public function phForm($id)
+    {
         $personalhomecare = PersonalHomecare::find($id);
-        
+
         return view('personal_homecare', compact('personalhomecare'));
     }
 
@@ -181,25 +184,142 @@ class PersonalHomecareController extends Controller
             ->where("active", true)
             ->get();
 
+        $personalhomecare = PersonalHomecare::where("id", $request->id)
+            ->with("checklistOut")
+            ->first();
+
         $provinces = RajaOngkir_Province::select(
                 "province_id AS id",
                 "province AS name",
             )
             ->get();
 
-        $personalhomecare = PersonalHomecare::where("id", $request->id)->first();
+        $cities = RajaOngkir_City::select(
+                "city_id AS id",
+                DB::raw("CONCAT(type, ' ', city_name) AS name"),
+            )
+            ->where("province_id", $personalhomecare->province_id)
+            ->get();
+
+        $subdistricts = RajaOngkir_Subdistrict::select(
+                "subdistrict_id AS id",
+                "subdistrict_name AS name",
+            )
+            ->where("province_id", $personalhomecare->province_id)
+            ->get();
+
+        $phcProducts = PersonalHomecareProduct::select(
+                "personal_homecare_products.id AS id",
+                "personal_homecare_products.code AS code",
+                "products.name AS name",
+            )
+            ->leftJoin(
+                "products",
+                "personal_homecare_products.product_id",
+                "=",
+                "products.id"
+            )
+            ->where("personal_homecare_products.branch_id", $personalhomecare->branch_id)
+            ->where("personal_homecare_products.active", true)
+            ->get(  );
 
         return view("admin.update_personal_homecare", compact(
             "personalhomecare",
             "branches",
             "csos",
             "provinces",
+            "cities",
+            "subdistricts",
+            "phcProducts",
         ));
     }
 
     public function update(Request $request)
     {
-        //
+        DB::beginTransaction();
+
+        try {
+            // UPDATE PERSONAL HOMECARE
+            $personalHomecare = PersonalHomecare::where("id", $request->id)->first();
+            $personalHomecare->fill($request->only(
+                "schedule",
+                "name",
+                "phone",
+                "address",
+                "province_id",
+                "city_id",
+                "subdistrict_id",
+                "branch_id",
+                "cso_id",
+                "ph_product_id",
+            ));
+
+            if ($request->hasFile("id_card_image")) {
+                $timestamp = (string) time();
+                $fileName = $timestamp
+                    . "."
+                    . $request->file("id_card_image")->getClientOriginalExtension();
+
+                $request->file("id_card_image")->move("sources/phc", $fileName);
+
+                $personalHomecare->id_card = $fileName;
+            }
+
+            $personalHomecare->save();
+
+            // UPDATE PERSONAL HOMECARE CHECKLIST OUT
+            $phcChecklistOut = PersonalHomecareChecklist::where(
+                    "id",
+                    $personalHomecare->checklist_out
+                )
+                ->first();
+
+            $condition["completeness"][] = $request->input("completeness");
+            if ($request->has("other_completeness")) {
+                $condition["other"] = $request->input("other_completeness");
+            }
+            $condition["machine"] = $request->input("machine_condition");
+            $condition["physical"] = $request->input("physical_condition");
+            $phcChecklistOut->condition = $condition;
+
+            $imageArray = $phcChecklistOut->image;
+            $userId = Auth::user()["id"];
+            $path = "sources/phc-checklist";
+            if ($request->hasFile("product_photo_1")) {
+                $fileName = time()
+                    . "_"
+                    . $userId
+                    . "_"
+                    . "1."
+                    . $request->file("product_photo_1")->getClientOriginalExtension();
+                $request->file("product_photo_1")->move($path, $fileName);
+                $imageArray[0] = $fileName;
+            }
+
+            if ($request->hasFile("product_photo_2")) {
+                $fileName = time()
+                    . "_"
+                    . $userId
+                    . "_"
+                    . "2."
+                    . $request->file("product_photo_2")->getClientOriginalExtension();
+                $request->file("product_photo_2")->move($path, $fileName);
+                $imageArray[1] = $fileName;
+            }
+
+            $phcChecklistOut->image = $imageArray;
+            $phcChecklistOut->save();
+
+            DB::commit();
+
+            return redirect()
+                ->route("detail_personal_homecare", ["id" => $request->id])
+                ->with("success", "Personal Homecare has been successfully updated.");
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return response()->json(["error" => $e->getMessage()], 500);
+        }
     }
 
     public function detail(Request $request){
