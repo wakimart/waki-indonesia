61ae40283e3e6d3b5fb2ed89cfb7546ad7f7b8f0
andrerahardjo97 <20762999+andrerahardjo97@users.noreply.github.com>
2021-09-06 10:21:46 +0700

    feat(Warehouse): Add

--- app/Http/Controllers/WarehouseController.php
+++ app/Http/Controllers/WarehouseController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\RajaOngkir_Province;
+use App\Warehouse;
 use Illuminate\Http\Request;
 
 class WarehouseController extends Controller
@@ -13,12 +15,39 @@ class WarehouseController extends Controller
 
     public function create()
     {
-        //
+        $parentWarehouses = Warehouse::select("id", "code", "name")
+            ->where("parent_warehouse_id", null)
+            ->orderBy("code")
+            ->get();
+
+        $provinces = RajaOngkir_Province::select(
+                "province_id AS id",
+                "province AS name",
+            )
+            ->get();
+
+        return view("admin.add_warehouse", compact(
+            "parentWarehouses",
+            "provinces",
+        ));
     }
 
     public function store(Request $request)
     {
-        //
+        Warehouse::create($request->only(
+            "code",
+            "name",
+            "address",
+            "province_id",
+            "city_id",
+            "subdistrict_id",
+            "description",
+            "parent_warehouse_id",
+        ));
+
+        return redirect()
+            ->route("add_warehouse")
+            ->with("success", "Warehouse successfully added.");
     }
 
     public function show(Request $request)
