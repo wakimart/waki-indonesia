56a2e0b377d00361b78d4ce77e10c0c85674300b
frezznezz <jatmikogunawan@gmail.com>
2022-03-11 18:27:33 +0700

    fix layout button delete, add info to data table filter

--- resources/views/admin/list_submission_form.blade.php
+++ resources/views/admin/list_submission_form.blade.php
@@ -28,6 +28,10 @@ if (isset($_GET["filter_type"])) {
     .select2-selection__arrow {
         top: 10px;
     }
+    .btn-delete {
+      background: transparent;
+      border: 0;
+    }
 </style>
 @endsection
 
@@ -152,7 +156,7 @@ if (isset($_GET["filter_type"])) {
             <div class="card">
                 <div class="card-body">
                     <h5 style="margin-bottom: 0.5em;">
-                        Total: {{ $submissions->total() }} data
+                        Total: {{ $submissions->total() }} data <b>{{$_GET["filter_type"]}}</b>
                     </h5>
                     <div class="table-responsive"
                         style="border: 1px solid #ebedf2;">
@@ -224,11 +228,11 @@ if (isset($_GET["filter_type"])) {
                                         {{-- @endcan --}}
                                         {{-- @can('delete-submission') --}}
                                             <td class="text-center">
-                                                <button class="btn-delete"
+                                                <button class="btn-delete p-0"
                                                     data-toggle="modal"
                                                     data-target="#deleteDoModal"
                                                     value="{{ route('delete_submission_form', ['id' => $submission->id]) }}">
-                                                    <i class="mdi mdi-delete" style="font-size: 24px; color: #fe7c96;"></i>
+                                                    <i class="mdi mdi-delete" style="font-size: 24px; color: #f94569;"></i>
                                                 </button>
                                             </td>
                                         {{-- @endcan --}}
@@ -286,6 +290,8 @@ if (isset($_GET["filter_type"])) {
 @section('script')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js" defer></script>
 <script type="application/javascript">
+
+
 document.addEventListener("DOMContentLoaded", function () {
     try {
         $("#filter-cso").select2();
