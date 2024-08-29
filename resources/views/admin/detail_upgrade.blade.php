1ba5d33f1cb44a8dd4384cdb0dfdb18621ddc1fc
Albertus Tjahjono <albertus.tjahjono95@gmail.com>
2024-08-29 17:23:13 +0700

    :feat

--- resources/views/admin/detail_upgrade.blade.php
+++ resources/views/admin/detail_upgrade.blade.php
@@ -478,7 +478,7 @@ $menu_item_second = "detail_upgrade_form";
                                 <h2>Upgrade Status</h2>
                             </div>
                             <div class="row justify-content-center">
-                                <form id="actionAdd"
+                                <form id="actionAdd_stockDR"
                                     class="forms-sample"
                                     method="POST"
                                     action="<?php echo route("update_upgrade_status"); ?>">
@@ -509,4 +509,143 @@ $menu_item_second = "detail_upgrade_form";
         <?php endif; ?>
 	</div>
 </div>
+
+<!-- Error modal -->
+<div class="modal fade"
+    id="error-modal"
+    tabindex="-1"
+    role="dialog"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button"
+                    class="close"
+                    data-dismiss="modal"
+                    aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div id="error-modal-desc"></div>
+        </div>
+    </div>
+</div>
+<!-- End Modal View -->
+@endsection
+
+@section('script')
+	@if($upgrade->acceptance['oldproduct_id'] != null)
+		<script>
+			var urlOffline = "{{ env('OFFLINE_URL_2') }}"
+			$(document).ready(function () {
+				var networkValue;
+		        function testNetwork(networkValue, response){
+		            // response();
+		            $.ajax({
+		                method: "post",
+		                url: `${urlOffline}/api/end-point-for-check-status-network`,
+		                dataType: 'json',
+		                contentType: 'application/json',
+		                processData: false,
+		                headers: {
+		                    "api-key": "{{ env('API_KEY') }}",
+		                },
+		                success: response,
+		                error: function(xhr, status, error) {
+		                    var modal = `
+		                        <div class="modal-body">
+		                            <h5 class="modal-title text-center">${xhr.responseJSON.status}</h5>
+		                            <hr>
+		                            <p class="text-center">${xhr.responseJSON.message}</p>
+		                        </div>
+		                    `;
+		                    $('#error-modal-desc').html(modal);
+		                    $('#error-modal').modal("show");
+		                }
+		            });
+		        };
+
+		        $('#actionAdd_stockDR').submit(function (e) {
+		        	e.preventDefault();
+		        	console.log(e.nativeEvent);
+		        	var formSerialize = $(this).serializeArray();
+		        	testNetwork(networkValue, function (val) {
+		        		console.log("masuk");
+		        		@php
+			        		$from_warehouse_id = null; // from warehouse display
+			        		$to_warehouse_id = null; // to warehouse ready
+		        			if($upgrade->acceptance['area'] == 'jakarta'){
+				        		$from_warehouse_id = 32;
+				        		$to_warehouse_id = 29;
+		        			}
+		        			elseif($upgrade->acceptance['area'] == 'surabaya'){
+				        		$from_warehouse_id = 35;
+				        		$to_warehouse_id = 4;
+		        			}
+		        		@endphp
+		                var dataPost = {
+		                	"acc_upgrade_id": "{{ $upgrade->acceptance['id'] }}",
+						    "date": "{{ date('Y-m-d') }}",
+						    "type": "out",
+						    "from_warehouse_id": {{ $from_warehouse_id }},
+						    "to_warehouse_id": {{ $to_warehouse_id }},
+						    "product_id": {{ $upgrade->acceptance['oldproduct_id'] }},
+						    "qty": 1,
+						    "warehouse_type": "warehouse"
+		                };
+
+		                $.ajax({
+	                        method: "post",
+	                        url: `${urlOffline}/api/store_stock_api/add`,
+	                        data: dataPost,
+	                        beforeSend: function() {
+	                            $('#btn-edit-status-order').html('loading...');
+	                            $('#btn-edit-status-order').attr('disabled', true);
+	                        },
+	                        success: function(res){
+	                            if(res.status == 'success'){
+	                                $('#actionAdd').submit();
+	                            }else{
+	                                var modal = `
+	                                    <div class="modal-body">
+	                                        <h5 class="modal-title text-center">${res.status}</h5>
+	                                        <hr>
+	                                        <p class="text-center">${res.message}</p>
+	                                    </div>
+	                                `
+	                                $('#modal-change-status').modal("hide")
+	                                $('.modal-backdrop').remove();
+	                                $('#error-modal-desc').html(modal)
+	                                $('#error-modal').modal("show")
+
+	                                $('#btn-edit-status-order').html('Yes');
+	                                $('#btn-edit-status-order').attr('disabled', false);
+	                            }
+	                        },
+	                        error: function(xhr){
+	                            var modal = `
+	                                <div class="modal-body">
+	                                    <h5 class="modal-title text-center">${xhr.responseJSON.status}</h5>
+	                                    <hr>
+	                                    <p class="text-center">${xhr.responseJSON.message}</p>
+	                                </div>
+	                            `
+	                            $('#modal-change-status').modal("hide")
+	                            $('.modal-backdrop').remove();
+	                            $('#error-modal-desc').html(modal)
+	                            $('#error-modal').modal("show")
+
+	                            $('#btn-edit-status-order').html('Yes');
+	                            $('#btn-edit-status-order').attr('disabled', false);
+	                        }
+
+	                    });
+
+			        	return false;
+		        	});
+		        	return false;
+		        });
+			})
+		</script>
+	@endif
 @endsection
